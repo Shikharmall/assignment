ed1093267901a8735e2f6087bdbcbfa8217aae44
Shikhar Mall <shmall.21.2020@gmail.com>
2024-01-08 23:15:21 +0530

    first commit of project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import "./css/style.css";
+
+// Import pages
+import NotFound from "./components/NotFound";
+import DataTable from "./pages/DataTable";
+
+function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Routes>
+        <Route exact path="/datatable" element={<DataTable/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
